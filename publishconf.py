c5385996f188f11f31aed03f7bb829f17c8d829c
Perceu Bertoletti <perceubertoletti@gmail.com>
2023-05-20 15:39:29 -0300

    fix to github pages deploy

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://pythonsul.dev'
+SITEURL = 'https://pythonsul.github.io/PythonSul-Site/'
 THEME = 'theme'
 RELATIVE_URLS = False
 
