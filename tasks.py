79cb08890d2e3153f27a2f1fd841b65fe376703f
Perceu Bertoletti <perceubertoletti@gmail.com>
2023-08-05 13:42:57 -0300

    adicionado c name corretamente

--- tasks.py
+++ tasks.py
@@ -117,4 +117,4 @@ def gh_pages(c):
     preview(c)
     c.run('ghp-import -b {github_pages_branch} '
           '-m {commit_message} '
-          '{deploy_path} -p -c pycaxias.com.br'.format(**CONFIG))
+          '{deploy_path} -p -c pythonsul.dev'.format(**CONFIG))
