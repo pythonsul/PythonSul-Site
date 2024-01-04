b59306b4f9663d7b8d318f6eee9dfdd991dec70d
Talita Rossari <rossaritalita@gmail.com>
2024-01-04 18:48:46 -0300

    feat: melhorando performance

--- theme/static/js/main.js
+++ theme/static/js/main.js
@@ -60,7 +60,7 @@ jQuery(document).ready(function( $ ) {
       'id': ''
     });
     $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
+    $('body').prepend('<button type="button" aria-label="Botão de menu" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
     $('body').append('<div id="mobile-body-overly"></div>');
     $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
 
